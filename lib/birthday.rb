e0fad4e57df02966f1315812b6356589e659fc75
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-15 15:33:41 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,5 +10,14 @@ def happy_birthday(birthday_kids)
   end
 end
 
-
+ def happy_birthday(birthday_kids)
+    birthday_kids.each do |kid,age|
+      if (age < 13 || !@age_appropriate) 
+        puts "Happy Birthday #{kid}! You are now #{age} years old!"
+      else
+        puts "#{kid} you are too old for this."
+      end
+    end 
+  end 
+end
  
