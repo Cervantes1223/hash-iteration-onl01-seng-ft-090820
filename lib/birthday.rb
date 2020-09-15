b706a18a19261a81d8753ea0c4aa126f59271316
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-15 15:36:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,7 +4,11 @@
   "Amanda" => 27
  }
 
-
+def age_appropriate_birthday(birthday_kids)
+  young, old = birthday_kids.parititon { |name, age| age < 13 }
+  happy_birthday(young)
+  old.each { |name, age| puts "#{kid} you are too old for this." }
+end
 
  
  
