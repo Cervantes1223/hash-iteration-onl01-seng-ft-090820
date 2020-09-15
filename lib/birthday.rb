d30342759d83cac76504bbea26ca150202f038a5
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-15 15:22:13 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    elsif birthday_kids["Amanda"] 
+    birthday_kids["Amanda"] 
     puts "You are too old for this."
   end 
  
